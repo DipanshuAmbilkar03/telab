d2c6258e6b39915c434d4861cb7f051552f62bef
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2025-11-10 00:56:46 +0530

    dns

--- CN/lab11/dns_lookup.py
+++ CN/lab11/dns_lookup.py
@@ -8,12 +8,20 @@ choice = input("Enter choice (1 or 2): ")
 if choice == "1":
     domain = input("Enter Domain Name (e.g., google.com): ")
     try:
-        ip = socket.gethostbyname(domain)
-        # info = socket.getaddrinfo(domain, None)
-        # for result in info:
-        #     print(result[4][0])
+        info = socket.getaddrinfo(domain, None)
+        print(f"\nIP addresses for {domain}:\n")
+        for result in info:
+            family = result[0]
+            ip_addr = result[4][0]
 
-        print(f"IP address of {domain} is: {ip}")
+            # Check address family and print accordingly
+            if family == socket.AF_INET:
+                print(f"IPv4: {ip_addr}")
+            elif family == socket.AF_INET6:
+                print(f"IPv6: {ip_addr}")
+
+        print("\n DNS Lookup Successful!\n")
+
     except socket.gaierror:
         print("Invalid domain name or DNS lookup failed!")
 
@@ -21,7 +29,9 @@ elif choice == "2":
     ip = input("Enter IP Address (e.g., 8.8.8.8): ")
     try:
         domain = socket.gethostbyaddr(ip)
-        print(f"Domain name for {ip} is: {domain[0]}")
+        print(f"\nIPv4/IPv6 Address: {ip}")
+        print(f"Domain name for this IP is: {domain[0]}")
+        print("\n Reverse DNS Lookup Successful!\n")
     except socket.herror:
         print("Reverse DNS lookup failed or invalid IP address!")
 
